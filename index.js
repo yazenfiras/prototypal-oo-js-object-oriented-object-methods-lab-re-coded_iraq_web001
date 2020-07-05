8d7da48b0f555ad9ffafa33bd8abb7de2b7a6364
yazen firas <psyazen3@gmail.com>
2020-07-05 19:57:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-function Scooter(year, color, model){
-  this.year = year;
-  this.color = color;
-  this.model = model
+function BoardMember(name, homeState, training){
+  this.name = name
+  this.homeState = homeState
+  this.training = training
 }
 
-function Driver(name, age, experience){
-  this.name = name;
-  this.age = age;
-  this.experience = experience;
+BoardMember.prototype.veto = function(){
+  return 'No, I must disagree'
 }
 
-function PickupLocation(address, city){
-  this.address = address;
-  this.city = city;
+BoardMember.prototype.approve = function(){
+  return 'You can do that!'
+}
+
+BoardMember.prototype.doCharity = function(){
+  return "I like to help people."
+}
+
+BoardMember.prototype.releasePressStatement = function(){
+  return "You will see great things from Scuber."
+}
+
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 }
